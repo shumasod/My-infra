f2010dabecbb9c9a67ee99c06607ec31e268037d
ShuBox <64628603+shumasod@users.noreply.github.com>
2023-12-14 23:17:46 +0900

    Update autossh.sh

--- autossh.sh
+++ autossh.sh
@@ -10,3 +10,27 @@ send \"${PW}\n\"
 expect \"$\"
 exit 0
 "
+
+
+#!/usr/bin/expect
+
+set PW "Password"
+
+set timeout 5
+
+spawn env LANG=C /usr/bin/ssh "servername"
+expect {
+    "(yes/no)?" {
+        send "yes\n"
+        exp_continue
+    }
+    "password:" {
+        send "${PW}\n"
+    }
+}
+
+expect {
+    "\\\$" {
+        exit 0
+    }
+}

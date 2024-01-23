7e9041f75c8c572bdd13c588eab3f3fa8a7ceaac
ShuBox <64628603+shumasod@users.noreply.github.com>
2024-01-23 23:16:44 +0900

    Create Hakkinnguhuuuu.sh

---
+++ pc/Hakkinnguhuuuu.sh
@@ -0,0 +1,22 @@
+# ハッキングしたように見せかけるPowershellスクリプト
+
+# ランダムな文字列を生成
+$randStr = [System.Random]::NextString(100)
+
+# ランダムなファイル名を生成
+$randFile = [System.IO.Path]::GetRandomFileName(".", ".txt")
+
+# ランダムなテキストを生成
+$randText = [System.Random]::NextString(1000)
+
+# テキストをファイルに書き込み
+[System.IO.File]::WriteAllText($randFile, $randText)
+
+# ファイルを開く
+$process = Start-Process -FilePath $randFile
+
+# プロセスを待機
+$process.WaitForExit()
+
+# メッセージを出力
+Write-Host "ハッキングが完了しました。"

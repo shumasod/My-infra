d2f77ab7e154704d2cf3546c50a742f7f52f85e1
ShuBox <64628603+shumasod@users.noreply.github.com>
2024-03-03 19:14:21 +0900

    Update create.sh

--- DB/create.sh
+++ DB/create.sh
@@ -1,7 +1,3 @@
-service mysql start
-mysql --defaults-extra-file=/etc/mysql/my.cnf -u root -e "drop database if exists dbname;"
-mysql --defaults-extra-file=/etc/mysql/my.cnf -u root -e "create database dbname;"
-mysql --defaults-extra-file=/etc/mysql/my.cnf -u root dbname < file.sql
 
 
 #!/bin/bash

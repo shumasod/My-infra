db1ba82d96ea0f0fe4da887b845d63601a879641
Shuma <shumasod@gmail.com>
2024-03-25 22:25:37 +0900

    鴨葱レボリューション

---
+++ kamonegi/Draft5.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# アスキーアートでうどんを表示する関数
+function show_udon() {
+  echo "＿人人人人人人＿"
+  echo "＞　うどん　　 ＜"
+  echo "￣Y^Y^Y^Y^Y^Y￣"
+}
+
+# 鴨葱うどんを表示する関数
+function show_kamonegi() {
+  echo "    　　　　 ＿＿＿_"
+  echo "    　　 ／　　　　 　 ＼"
+  echo "    　 /　　　●　　　　 \\"
+  echo "   ／　　　　　　　　　　　 ＼"
+  echo "　／　　　　　　　　　　　　 ＼"
+  echo "　｜ 　　　 ●　　●　　　 　 ｜"
+  echo "　｜　　　　　　　　　　　　 ｜"
+  echo "　｜　　　　　　　　　　　　 ｜"
+  echo "　｜　　　　　∩　∩　　　　 ｜"
+  echo "　｜　　　　　/ |　 | ＼　　 　 ｜"
+  echo "　｜　　　　　ｕ　ｕ　　　　 ｜"
+  echo "　｜　　　　　　　　　　　　 ｜"
+  echo "　｜　　　　　　　　　　　　 ｜"
+  echo "　｜　　　　　　　　　　　　 ｜"
+  echo "　｜　　　　　　　　　　　　 ｜"
+  echo "　｜　　　　　　　　　　　　 ｜"
+  echo "　｜　　　　　　　　　　　　 ｜"
+  echo "　｜　　　　　　　　　　　　 ｜"
+  echo "　｜　　　　　　　　　　　　 ｜"
+  echo "　｜　　　　　　　　　　　　 ｜"
+  echo "　｜　　　　　　　　　　　　 ｜"
+  echo "　｜　　　　　　　　　　　　 ｜"
+  echo "　｜　　　　　　　　　　　　 ｜"
+  echo "　 ＼＿＿＿＿＿＿＿＿＿／"
+
+  echo "**********************************"
+  echo "** 鴨葱うどん **"
+  echo "**********************************"
+
+  echo "  具材："
+  echo "    - うどんは200g"
+  echo "    - 鴨肉は25g"
+  echo "    - 九条ネギ"
+  echo "    - だし汁"
+
+  echo "  作り方："
+  echo "  1. 鍋にだし汁を入れて沸騰させる。"
+  echo "  2. うどんを袋から取り出し、鍋に入れる。"
+  echo "  3. うどんが柔らかくなったら、鴨肉と九条ネギを加える。"
+  echo "  4. 温まったら、器に盛り付けて完成。"
+
+  echo "**********************************"
+}
+
+# うどんを表示
+show_udon
+
+# 鴨葱うどんを表示
+show_kamonegi
